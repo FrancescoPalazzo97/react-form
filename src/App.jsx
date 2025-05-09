918dff8c54bcc21e5ea2e576eb24388e14e6772c
FrancescoPalazzo97 <francescopalazzo97@gmail.com>
2025-05-09 16:17:35 +0200

    [FEAT] Implement delete article

--- src/App.jsx
+++ src/App.jsx
@@ -1,31 +1,31 @@
-import initialArticles from './assets/articles.js'
-import { useState } from 'react'
+import initialArticles from './assets/articles.js';
+import { useState } from 'react';
 
 function App() {
-
-  const [newArticle, setNewArticle] = useState("")
-  const [articles, setArticles] = useState(initialArticles)
+  const [newArticle, setNewArticle] = useState("");
+  const [articles, setArticles] = useState(initialArticles);
 
   const addArticle = (e) => {
-    e.preventDefault()
-    console.log(`Nuovo articolo: ${newArticle}`)
-
+    e.preventDefault();
     const newArticleObj = {
-      id: articles[articles.length - 1].id + 1,
+      id: articles.length === 0 ? 1 : articles[articles.length - 1].id + 1,
       title: newArticle,
       description: "Descrizione dell'articolo",
       author: 'Giulia Neri',
       date: '2025-04-28',
       tags: ['Xbox', 'Game Pass', 'Recensioni']
-    }
+    };
+    setArticles([...articles, newArticleObj]);
+    setNewArticle("");
+  };
 
-    setArticles([...articles, newArticleObj])
-    setNewArticle("")
-  }
+  const handleDeleteElement = (id) => {
+    const filteredArticles = articles.filter(article => article.id !== id);
+    setArticles(filteredArticles);
+  };
 
   return (
     <>
-
       <header>
         <div className="container">
           <h1>React Form</h1>
@@ -39,27 +39,37 @@ function App() {
                 type="text"
                 placeholder="Inserisci titolo"
                 value={newArticle}
-                onChange={e => setNewArticle(e.target.value)} />
+                onChange={(e) => setNewArticle(e.target.value)}
+              />
               <button>invia</button>
             </div>
           </form>
           <div className="articles-container">
-            {articles.map(article => (
+            {articles.map((article) => (
               <div className="article-card" key={article.id}>
                 <div className="article-title">
                   <h2>{article.title}</h2>
                 </div>
-                <p>{article.description}</p>
-                <p>{article.author}</p>
-                <p>{article.date}</p>
+                <div className="article-body">
+                  <p>{article.description}</p>
+                  <p>{article.author}</p>
+                  <p>{article.date}</p>
+                  <div className="tags">
+                    {article.tags.map((tag) => (
+                      <span key={tag}>{tag}</span>
+                    ))}
+                  </div>
+                  <button onClick={() => handleDeleteElement(article.id)}>
+                    elimina
+                  </button>
+                </div>
               </div>
             ))}
           </div>
         </div>
-      </main >
-
+      </main>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
